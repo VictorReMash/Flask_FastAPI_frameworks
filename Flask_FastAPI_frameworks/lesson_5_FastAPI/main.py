6975c2e78e6a1344ff978a1b5e0b9cd01eb01ede
VictorReMash <158801659+VictorReMash@users.noreply.github.com>
2024-08-12 06:47:10 +0500

    изучаем FastAPI next

--- Flask_FastAPI_frameworks/lesson_5_FastAPI/main.py
+++ Flask_FastAPI_frameworks/lesson_5_FastAPI/main.py
@@ -4,28 +4,28 @@ from fastapi import FastAPI
 from typing import Optional
 from pydantic import BaseModel
 
-# logging.basicConfig(level=logging.INFO)
-# logger = logging.getLogger(__name__)
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
 
 app = FastAPI()
 
-#
-# class Item(BaseModel):
-#     name: str
-#     description: Optional[str] = None
-#     price: float
-#     tax: Optional[float] = None
+
+class Item(BaseModel):
+    name: str
+    description: Optional[str] = None
+    price: float
+    tax: Optional[float] = None
 
 
 @app.get("/")
 async def read_root():
-    # logger.info("Отработал GET запрос.")
+    logger.info("Отработал GET запрос.")
     return {"Hello": "World"}
 
 
 @app.get("/items/{item_id}")
 async def read_item(item_id: int, q: str = None):
-    # logger.info("Отработал POST запрос.")
+    logger.info("Отработал POST запрос.")
     if q:
         return {"item_id": item_id, "q": q}
     return {"item_id": item_id}
@@ -33,13 +33,11 @@ async def read_item(item_id: int, q: str = None):
 
 @app.get("/users/{user_id}/orders/{order_id}")
 async def read_data(user_id: int, order_id: int):
-    # logger.info(f"Отработал PUT запрос для item id = {item_id}.")
+    logger.info(f"Отработал PUT запрос для item id = {user_id}.")
     return {"user_id": user_id, "order_id": order_id}
 
 
-#
-#
-# @app.delete("/items/{item_id}")
-# async def delete_item(item_id: int):
-#     logger.info(f"Отработал DELETE запрос для item id ={item_id}.")
-#     return {"item_id": item_id}
+@app.delete("/items/{item_id}")
+async def delete_item(item_id: int):
+    logger.info(f"Отработал DELETE запрос для item id ={item_id}.")
+    return {"item_id": item_id}
